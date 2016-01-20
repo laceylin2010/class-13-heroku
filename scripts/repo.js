04728cc4370f512c8dd909cb69a7e2d49c3375ac
Lacey Vu <laceylin2010@gmail.com>
2016-01-20 13:12:06 -0800

    initialized testing

--- scripts/repo.js
+++ scripts/repo.js
@@ -18,15 +18,15 @@
   // };
 
   repos.requestRepos = function(callback) {
-    $.ajax({
+    $.get({
       url: '/github/users/laceylin2010/repos' +
             '?per_page=100' +
             '&sort=updated',
-      type: 'GET',
-      success: function(data, message, xhr) {
-        repos.all = data;
       }
-    }).done(callback);
+      .done(function(data, message, xhr) {
+        repos.all = data;
+      })
+      .done(callback);
   };
 
   repos.with = function(attr) {
